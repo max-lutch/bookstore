fd22d41528bb5ddd4a92d947ebdeadeaa11f82d6
Max Lutch <lutchmax@gmail.com>
2025-02-21 20:46:24 +0200

    End of the 7th chapter. Do the GetBook function and a testf for it

--- bookstore.go
+++ bookstore.go
@@ -1,20 +1,18 @@
 package bookstore
 
-import "errors"
-
 // Book represents information about a book.
 type Book struct {
 	Title  string
 	Author string
 	Copies int
+	ID     int
 }
 
-// Buy takes a Book, decrements its number of copies, and returns the modified
-// book.
-func Buy(b Book) (Book, error) {
-	if b.Copies == 0 {
-		return Book{}, errors.New("no copies left")
+func GetBook(catalog []Book, ID int) Book {
+	for _, b := range catalog {
+		if b.ID == ID {
+			return b
+		}
 	}
-	b.Copies--
-	return b, nil
+	return Book{}
 }
--- bookstore_test.go
+++ bookstore_test.go
@@ -3,43 +3,25 @@ package bookstore_test
 import (
 	"bookstore"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestBook(t *testing.T) {
+func TestGetBook(t *testing.T) {
 	t.Parallel()
-	_ = bookstore.Book{
-		Title:  "Spark Joy",
-		Author: "Marie Kondō",
-		Copies: 2,
+	catalog := []bookstore.Book{
+		{
+			ID:    1,
+			Title: "For the Love of Go",
+		},
+		{
+			ID:    2,
+			Title: "The Power of Go: Tools",
+		},
 	}
-}
-func TestBuy(t *testing.T) {
-	t.Parallel()
-	b := bookstore.Book{
-		Title:  "Spark Joy",
-		Author: "Marie Kondo",
-		Copies: 2,
-	}
-	want := 1
-	result, err := bookstore.Buy(b)
-	if err != nil {
-		t.Fatal(err)
-	}
-	got := result.Copies
-	if want != got {
-		t.Errorf("want %d, got %d", want, got)
-	}
-}
-
-func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
-	t.Parallel()
-	b := bookstore.Book{
-		Title:  "Spark Joy",
-		Author: "Marie Kondo",
-		Copies: 0,
-	}
-	_, err := bookstore.Buy(b)
-	if err == nil {
-		t.Errorf("want error buying from zero copies, got nil")
+	want := bookstore.Book{ID: 1, Title: "For the Love of Go"}
+	got := bookstore.GetBook(catalog, 1)
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
 	}
 }
